174d34961dd718fb12e20df4628290c82dbcb770
Lopes-Rodrigo <digonunes943@gmail.com>
2024-10-09 16:02:35 -0300

    atualizacoes dvfvdv

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -1,12 +1,14 @@
-// screens/WelcomeScreen.js
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {Image} from 'expo-image';
 export default function WelcomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.logo}>ErrOops</Text>
-      <View style={styles.imagePlaceholder} />
+      <Image 
+        source={require('../../assets/iconeErrOops.png')}  // Substitua pelo link da sua imagem ou use require para imagens locais
+        style={styles.image}
+      />
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
         <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
@@ -16,38 +18,39 @@ export default function WelcomeScreen({ navigation }) {
     </View>
   );
 }
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#fff',
-    },
-    logo: {
-      fontSize: 40,
-      color: '#8a0b07',
-      marginBottom: 20,
-    },
-    imagePlaceholder: {
-      width: 100,
-      height: 100,
-      backgroundColor: '#8a0b07',
-      borderRadius: 50,
-      marginBottom: 50,
-    },
-    button: {
-      backgroundColor: '#8a0b07',
-      paddingVertical: 15,
-      paddingHorizontal: 40,
-      borderRadius: 25,
-      marginBottom: 20,
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 18,
-    },
-    loginText: {
-      color: '#8a0b07',
-      textDecorationLine: 'underline',
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    fontSize: 40,
+    color: '#8a0b07',
+    marginBottom: 20,
+  },
+  image: {
+    marginTop:20,
+    width: 600,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 50,
+  },
+  button: {
+    backgroundColor: '#8a0b07',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 25,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  loginText: {
+    color: '#8a0b07',
+    textDecorationLine: 'underline',
+  },
+});
